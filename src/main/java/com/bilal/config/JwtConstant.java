e6eb99f27d5ce44a154186abf085e8e0a27539f0
bilal1960 <bilal45001@hotmail.com>
2024-04-01 04:05:04 +0200

    starting to code security token access

---
+++ src/main/java/com/bilal/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.bilal.config;
+
+public class JwtConstant {
+
+    public static final String JWT_SECRET="ahflvndjfdsksknrogsjlkngssknvqdhidgfkldbdgeyfndgfzmlojlaihfpklfdknhdoskljshb";
+
+    public  static  final  String JWT_HEADER="authorization";
+}
